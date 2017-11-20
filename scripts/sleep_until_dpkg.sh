88126f074bfb8adc5e4913319aff2a17d6821d07
James Webber <james.webber@czbiohub.org>
2017-11-20 14:52:32 -0800

    refactored the sleep/dpkg code into its own script because I want to use it

---
+++ scripts/sleep_until_dpkg.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "sleeping 30 sec"; sleep 30
+for try in {1..100}; do if [[ $try -gt 1 ]]; then echo "sleeping 10 sec"; sleep 10; fi; sudo dpkg --configure -a || continue; break; done
+sudo apt-get -f install
+exit 0
