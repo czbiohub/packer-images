187d6411a83f9169cc8ba7f55a16cf1636c422dd
Ryan Leenay <ryan.leenay@czbiohub.org>
2017-11-22 19:38:33 -0800

    Update tophat.sh

--- scripts/tophat.sh
+++ scripts/tophat.sh
@@ -3,26 +3,24 @@
 wget http://ccb.jhu.edu/software/tophat/downloads/tophat-2.1.1.Linux_x86_64.tar.gz
 tar -xzf tophat-2.1.1.Linux_x86_64.tar.gz 
 
-mkdir $HOME/bin
-
-sudo cp /home/ubuntu/tophat-2.1.1.Linux_x86_64/tophat $HOME/bin
-sudo cp /home/ubuntu/tophat-2.1.1.Linux_x86_64/bam2fastx $HOME/bin
-sudo cp /home/ubuntu/tophat-2.1.1.Linux_x86_64/bam_merge $HOME/bin
-sudo cp /home/ubuntu/tophat-2.1.1.Linux_x86_64/bed_to_juncs $HOME/bin
-sudo cp /home/ubuntu/tophat-2.1.1.Linux_x86_64/contig_to_chr_coords $HOME/bin
-sudo cp /home/ubuntu/tophat-2.1.1.Linux_x86_64/fix_map_ordering $HOME/bin
-sudo cp /home/ubuntu/tophat-2.1.1.Linux_x86_64/gtf_juncs $HOME/bin
-sudo cp /home/ubuntu/tophat-2.1.1.Linux_x86_64/gtf_to_fasta $HOME/bin
-sudo cp /home/ubuntu/tophat-2.1.1.Linux_x86_64/juncs_db $HOME/bin
-sudo cp /home/ubuntu/tophat-2.1.1.Linux_x86_64/long_spanning_reads $HOME/bin
-sudo cp /home/ubuntu/tophat-2.1.1.Linux_x86_64/prep_reads $HOME/bin
-sudo cp /home/ubuntu/tophat-2.1.1.Linux_x86_64/sam_juncs $HOME/bin
-sudo cp /home/ubuntu/tophat-2.1.1.Linux_x86_64/samtools_0.1.18 $HOME/bin
-sudo cp /home/ubuntu/tophat-2.1.1.Linux_x86_64/segment_juncs $HOME/bin
-sudo cp /home/ubuntu/tophat-2.1.1.Linux_x86_64/sra_to_solid $HOME/bin
-sudo cp /home/ubuntu/tophat-2.1.1.Linux_x86_64/tophat2 $HOME/bin
-sudo cp /home/ubuntu/tophat-2.1.1.Linux_x86_64/tophat-fusion-post $HOME/bin
-sudo cp /home/ubuntu/tophat-2.1.1.Linux_x86_64/tophat_reports $HOME/bin
+sudo cp /home/ubuntu/tophat-2.1.1.Linux_x86_64/tophat /usr/local/bin
+sudo cp /home/ubuntu/tophat-2.1.1.Linux_x86_64/bam2fastx /usr/local/bin
+sudo cp /home/ubuntu/tophat-2.1.1.Linux_x86_64/bam_merge /usr/local/bin
+sudo cp /home/ubuntu/tophat-2.1.1.Linux_x86_64/bed_to_juncs /usr/local/bin
+sudo cp /home/ubuntu/tophat-2.1.1.Linux_x86_64/contig_to_chr_coords /usr/local/bin
+sudo cp /home/ubuntu/tophat-2.1.1.Linux_x86_64/fix_map_ordering /usr/local/bin
+sudo cp /home/ubuntu/tophat-2.1.1.Linux_x86_64/gtf_juncs /usr/local/bin
+sudo cp /home/ubuntu/tophat-2.1.1.Linux_x86_64/gtf_to_fasta /usr/local/bin
+sudo cp /home/ubuntu/tophat-2.1.1.Linux_x86_64/juncs_db /usr/local/bin
+sudo cp /home/ubuntu/tophat-2.1.1.Linux_x86_64/long_spanning_reads /usr/local/bin
+sudo cp /home/ubuntu/tophat-2.1.1.Linux_x86_64/prep_reads /usr/local/bin
+sudo cp /home/ubuntu/tophat-2.1.1.Linux_x86_64/sam_juncs /usr/local/bin
+sudo cp /home/ubuntu/tophat-2.1.1.Linux_x86_64/samtools_0.1.18 /usr/local/bin
+sudo cp /home/ubuntu/tophat-2.1.1.Linux_x86_64/segment_juncs /usr/local/bin
+sudo cp /home/ubuntu/tophat-2.1.1.Linux_x86_64/sra_to_solid /usr/local/bin
+sudo cp /home/ubuntu/tophat-2.1.1.Linux_x86_64/tophat2 /usr/local/bin
+sudo cp /home/ubuntu/tophat-2.1.1.Linux_x86_64/tophat-fusion-post /usr/local/bin
+sudo cp /home/ubuntu/tophat-2.1.1.Linux_x86_64/tophat_reports /usr/local/bin
 
 sudo rm /home/ubuntu/tophat-2.1.1.Linux_x86_64.tar.gz
 sudo rm -r /home/ubuntu/tophat-2.1.1.Linux_x86_64
