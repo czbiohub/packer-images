f8f9d4e260eb43ab0bd0866addf27858d587fbe0
Ryan Leenay <ryan.leenay@czbiohub.org>
2017-11-22 19:35:15 -0800

    Update FastXtoolkit.sh

--- scripts/FastXtoolkit.sh
+++ scripts/FastXtoolkit.sh
@@ -11,28 +11,27 @@ cd $version
 
 currdir=$(pwd)
 
-mkdir $HOME/bin
 
-sudo cp $currdir/fasta_clipping_histogram.pl $HOME/bin
-sudo cp $currdir/fasta_formatter $HOME/bin
-sudo cp $currdir/fasta_nucleotide_changer $HOME/bin
-sudo cp $currdir/fastq_masker $HOME/bin
-sudo cp $currdir/fastq_quality_boxplot_graph.sh $HOME/bin
-sudo cp $currdir/fastq_quality_converter $HOME/bin
-sudo cp $currdir/fastq_quality_filter $HOME/bin
-sudo cp $currdir/fastq_quality_trimmer $HOME/bin
-sudo cp $currdir/fastq_to_fasta $HOME/bin
-sudo cp $currdir/fastx_artifacts_filter $HOME/bin
-sudo cp $currdir/fastx_barcode_splitter.pl $HOME/bin
-sudo cp $currdir/fastx_clipper $HOME/bin
-sudo cp $currdir/fastx_collapser $HOME/bin
-sudo cp $currdir/fastx_nucleotide_distribution_graph.sh $HOME/bin
-sudo cp $currdir/fastx_nucleotide_distribution_line_graph.sh $HOME/bin
-sudo cp $currdir/fastx_quality_stats $HOME/bin
-sudo cp $currdir/fastx_renamer $HOME/bin
-sudo cp $currdir/fastx_reverse_complement $HOME/bin
-sudo cp $currdir/fastx_trimmer $HOME/bin
-sudo cp $currdir/fastx_uncollapser $HOME/bin
+sudo cp $currdir/fasta_clipping_histogram.pl /usr/local/bin
+sudo cp $currdir/fasta_formatter /usr/local/bin
+sudo cp $currdir/fasta_nucleotide_changer /usr/local/bin
+sudo cp $currdir/fastq_masker /usr/local/bin
+sudo cp $currdir/fastq_quality_boxplot_graph.sh /usr/local/bin
+sudo cp $currdir/fastq_quality_converter /usr/local/bin
+sudo cp $currdir/fastq_quality_filter /usr/local/bin
+sudo cp $currdir/fastq_quality_trimmer /usr/local/bin
+sudo cp $currdir/fastq_to_fasta /usr/local/bin
+sudo cp $currdir/fastx_artifacts_filter /usr/local/bin
+sudo cp $currdir/fastx_barcode_splitter.pl /usr/local/bin
+sudo cp $currdir/fastx_clipper /usr/local/bin
+sudo cp $currdir/fastx_collapser /usr/local/bin
+sudo cp $currdir/fastx_nucleotide_distribution_graph.sh /usr/local/bin
+sudo cp $currdir/fastx_nucleotide_distribution_line_graph.sh /usr/local/bin
+sudo cp $currdir/fastx_quality_stats /usr/local/bin
+sudo cp $currdir/fastx_renamer /usr/local/bin
+sudo cp $currdir/fastx_reverse_complement /usr/local/bin
+sudo cp $currdir/fastx_trimmer /usr/local/bin
+sudo cp $currdir/fastx_uncollapser /usr/local/bin
 
 
 
