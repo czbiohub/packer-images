e5d8b5811b49e05acc2510c0f4fda176bd6a51f6
James Webber <james.webber@czbiohub.org>
2017-12-05 15:23:26 -0800

    rearranged some deckchairs

--- scripts/nanopore.sh
+++ scripts/nanopore.sh
@@ -2,20 +2,10 @@
 
 export PATH=$HOME/anaconda/bin:$PATH
 
-git clone --recursive https://github.com/nanoporetech/pomoxis
-
 conda install virtualenv
-
-cd pomoxis/
-
-yes | sudo apt-get install libz-dev
-yes | sudo apt-get install libncurses5-dev
-yes | sudo apt-get install libhdf5-dev
-yes | sudo apt-get install libblas*
-
-CFLAGS="-I/usr/include/hdf5/serial" CPPFLAGS="-I/usr/include/hdf5/serial" make install
-
 conda install biopython
+
+echo "setting up albacore"
 
 wget -P /tmp/ https://mirror.oxfordnanoportal.com/software/analysis/ont_albacore-2.1.3-cp36-cp36m-manylinux1_x86_64.whl
 
@@ -23,5 +13,25 @@ conda create -n albacore python=3
 source activate albacore
 pip install /tmp/ont_albacore-2.1.3-cp36-cp36m-manylinux1_x86_64.whl 
 source deactivate
+
+echo "done with albacore setup"
+
+echo "installing pomoxis dependencies"
+
+yes | sudo apt-get install libz-dev
+yes | sudo apt-get install libncurses5-dev
+yes | sudo apt-get install libhdf5-dev
+yes | sudo apt-get install libblas*
+
+echo "setting up pomoxis"
+
+git clone --recursive https://github.com/nanoporetech/pomoxis
+cd pomoxis/
+
+echo "starting make"
+
+CFLAGS="-I/usr/include/hdf5/serial" CPPFLAGS="-I/usr/include/hdf5/serial" make install
+
+echo "done with pomoxis setup"
 
 exit 0
