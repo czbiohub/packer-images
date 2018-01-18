4b5a3e53f424a60f5313f08cf23d9ab889035854
James Webber <james.webber@czbiohub.org>
2018-01-18 15:52:37 -0800

    this works, cellranger is annoying though (requires manually updating the token)

--- scripts/bcl2fastq.sh
+++ scripts/bcl2fastq.sh
@@ -27,10 +27,16 @@ done
 
 yes | sudo apt-get install unzip
 
-wget -P /tmp/ https://support.illumina.com/content/dam/illumina-support/documents/downloads/software/bcl2fastq/bcl2fastq2-v2-20-0-linux-x86-64.zip
+wget --no-verbose -P /tmp/ https://support.illumina.com/content/dam/illumina-support/documents/downloads/software/bcl2fastq/bcl2fastq2-v2-20-0-linux-x86-64.zip
 
 unzip /tmp/bcl2fastq2-v2-20-0-linux-x86-64.zip -d /tmp/
 
 sudo alien -i /tmp/bcl2fastq2-v2.20.0.422-Linux-x86_64.rpm
 
 bcl2fastq --version
+
+# note: update with recent key from their website
+# https://support.10xgenomics.com/single-cell-gene-expression/software/downloads/latest
+wget --no-verbose -O /tmp/cellranger-2.1.0.tar.gz "http://cf.10xgenomics.com/releases/cell-exp/cellranger-2.1.0.tar.gz
+tar -xzf /tmp/cellranger-2.1.0.tar.gz -C /tmp
+sudo mv /tmp/cellranger-2.1.0/* /usr/local/bin/
