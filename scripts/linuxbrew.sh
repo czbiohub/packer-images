76aff5a8fc64e73e1ee7c51556841b7432eb1d36
olgabot <olga.botvinnik@gmail.com>
2018-02-07 16:43:00 -0800

    Add linuxbrew script

---
+++ scripts/linuxbrew.sh
@@ -0,0 +1,4 @@
+sudo apt-get install build-essential curl file git python-setuptools
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+brew install hello
+brew doctor
