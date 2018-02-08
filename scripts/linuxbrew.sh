3391daf0e287facb5061b9f4e60b7dc165b5d7f2
olgabot <olga.botvinnik@gmail.com>
2018-02-07 16:48:08 -0800

    Add exporting path and such

--- scripts/linuxbrew.sh
+++ scripts/linuxbrew.sh
@@ -1,4 +1,9 @@
 sudo apt-get install build-essential curl file git python-setuptools
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+echo 'export PATH="/home/ubuntu/.linuxbrew/bin:$PATH"' >>~/.profile
+echo 'export MANPATH="/home/ubuntu/.linuxbrew/share/man:$MANPATH"' >>~/.profile
+echo 'export INFOPATH="/home/ubuntu/.linuxbrew/share/info:$INFOPATH"' >>~/.profile
+export PATH="/home/ubuntu/.linuxbrew/bin:$PATH"
+brew install gcc
 brew install hello
 brew doctor
