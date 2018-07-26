6331def9210f13d35c2d6401a5557c7241b85dcd
Greg Dingle <gdingle@chanzuckerberg.com>
2018-07-26 12:32:51 -0700

    merge snp files

--- scripts/crispr-primer.sh
+++ scripts/crispr-primer.sh
@@ -14,16 +14,33 @@ export PATH=$HOME/anaconda/bin:$PATH
 conda create -n python2 anaconda python=2 && source activate python2
 conda install -c bioconda primer3 ucsc-twobittofa
 
+sudo apt-get install -y liblzo2-dev zlib1g-dev
+pip install fastinterval
+
 cd /mnt/data
 mkdir genome && cd genome
 wget http://hgdownload.cse.ucsc.edu/goldenPath/hg38/bigZips/hg38.2bit
 twoBitToFa hg38.2bit hg38.fa
 wget http://hgdownload.cse.ucsc.edu/goldenPath/mm10/bigZips/mm10.2bit
 twoBitToFa mm10.2bit mm10.fa
+wget http://hgdownload.cse.ucsc.edu/goldenPath/hg38/snp142Mask/chr*.subst.fa.gz
 ln -s /mnt/data/genome ~/genome
 
-sudo apt-get install -y liblzo2-dev zlib1g-dev
-pip install fastinterval
+cd /mnt/data/genome
+mkdir hg38snp142 && hg38snp142
+curl --remote-name-all http://hgdownload.cse.ucsc.edu/goldenPath/hg38/snp142Mask/chr[1-22].subst.fa.gz
+curl --remote-name-all http://hgdownload.cse.ucsc.edu/goldenPath/hg38/snp142Mask/chr{X,Y}.subst.fa.gz
+gzip -d *.gz
+# TODO: is order important???
+cat *.subst.fa > ../hg38.snp142.fa
+
+cd /mnt/data/genome
+mkdir mm10snp142 && mm10snp142
+curl --remote-name-all http://hgdownload.cse.ucsc.edu/goldenPath/mm10/snp142Mask/chr[1-19].subst.fa.gz
+curl --remote-name-all http://hgdownload.cse.ucsc.edu/goldenPath/mm10/snp142Mask/chr{X,Y}.subst.fa.gz
+gzip -d *.gz
+# TODO: is order important???
+cat *.subst.fa > ../mm10.snp142.fa
 
 # TODO: download mask snp142 from USCS and cat into one big file.
 # snp142mask/chr1.subst.fa.gz to chr22 plus X and Y
