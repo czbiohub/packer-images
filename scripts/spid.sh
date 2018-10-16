0f07d366a221a220e25a7fb171e3f60df1f3238b
Jack Kamm <jackkamm@gmail.com>
2018-10-16 19:50:06 +0000

    increase open connection limit for spid

--- scripts/spid.sh
+++ scripts/spid.sh
@@ -1,5 +1,13 @@
 #!/bin/bash -x
 
+# These files are necessary to increase the number of open file limits
+# https://superuser.com/a/1200818/166053
+sudo cp /tmp/common-session /etc/pam.d/common-session
+sudo cp /tmp/common-session-noninteractive /etc/pam.d/common-session-noninteractive
+sudo cp /tmp/limits.conf /etc/security/limits.conf
+sudo cp /tmp/user.conf /etc/systemd/user.conf
+sudo cp /tmp/system.conf /etc/systemd/system.conf
+
 export PATH=$HOME/anaconda/bin:$PATH
 
 conda install python=3.6
