06925530bf863567a0d2ff425d2b5359213ea428
hcanaj <hera.canaj@czbiohub.org>
2019-08-15 14:07:49 -0700

    Using different base image.

--- scripts/pacbio-postlaunch.sh
+++ scripts/pacbio-postlaunch.sh
@@ -2,7 +2,12 @@
 # During the packer build process for pac bio image - this script is
 # copied to /var/lib/cloud/scripts/per-instance and thus will be run
 # each time instance is launched
+sudo sh -c 'echo "127.0.0.1 $(cat /etc/hostname)" >> /etc/hosts'
 cd /mnt/data
 publicDNS="$(curl -s http://169.254.169.254/latest/meta-data/public-hostname)"
 sed -i "s/biohubisawesome/$publicDNS/g" smrtlink.config
-./smrtlink_7.0.1.66975.run --configfile smrtlink.config --batch
+runuser -l ubuntu -c "cd /mnt/data && ./smrtlink_7.0.1.66975.run --configfile smrtlink.config --batch"
+cd smrtlink/admin/bin
+runuser -l ubuntu -c "./services-start"
+sudo sh -c "echo 'SMRT LINK services ready. Go to ${publicDNS}:9090' > /etc/motd"
+wall "SMRTLINK services ready. Go to ${publicDNS}:9090"
--- scripts/pacbio.sh
+++ scripts/pacbio.sh
@@ -4,11 +4,17 @@ cd /mnt/data
 
 aws s3 cp s3://czbiohub-scripts/other_software/smrtlink_7.0.1.66975.zip .
 aws s3 cp s3://czbiohub-scripts/other_software/smrtlink.config .
-sudo apt-get install zip unzip
-gunzip smrtlink_7.0.1.66975.zip.gz
 unzip smrtlink_7.0.1.66975.zip
-ulimit -u 10000
-ulimit -n 10000
 
-sudo aws s3 cp s3://czbiohub-scripts/other_software/pacbio-postlaunch.sh /var/lib/cloud/scripts/per-instance
+sudo sh -c 'echo "* soft nproc 10000" >> /etc/security/limits.conf'
+sudo sh -c 'echo "* hard nproc 10000" >> /etc/security/limits.conf'
+sudo sh -c 'echo "* soft nofile 10000" >> /etc/security/limits.conf'
+sudo sh -c 'echo "* hard nofile 10000" >> /etc/security/limits.conf'
+
+sudo sh -c 'aws s3 cp s3://czbiohub-scripts/other_software/pacbio-postlaunch.sh /var/lib/cloud/scripts/per-instance'
 sudo chmod 755 /var/lib/cloud/scripts/per-instance/pacbio-postlaunch.sh
+
+sudo sh -c "echo 'SMRT Link services not ready.' > /etc/motd"
+
+sudo apt-get --purge remove postgresql
+sudo apt-get install postgresql-9.6
