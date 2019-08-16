a45d5d7395e27294aec8e8684f94d60453fe2121
Aaron McGeever <aaron.mcgeever@czbiohub.org>
2019-08-16 09:26:19 -0700

    Update pacbio.json
    Attach volumes of 1TB and also use smaller image for build where
    the volume name can be determined using packer.

--- scripts/pacbio-postlaunch.sh
+++ scripts/pacbio-postlaunch.sh
@@ -1,13 +1,17 @@
 #!/bin/bash -x
+
+set -e
+
 # During the packer build process for pac bio image - this script is
 # copied to /var/lib/cloud/scripts/per-instance and thus will be run
 # each time instance is launched
+
 sudo sh -c 'echo "127.0.0.1 $(cat /etc/hostname)" >> /etc/hosts'
 cd /mnt/data
 publicDNS="$(curl -s http://169.254.169.254/latest/meta-data/public-hostname)"
 sed -i "s/biohubisawesome/$publicDNS/g" smrtlink.config
 runuser -l ubuntu -c "cd /mnt/data && ./smrtlink_7.0.1.66975.run --configfile smrtlink.config --batch"
 cd smrtlink/admin/bin
-runuser -l ubuntu -c "./services-start"
+runuser -l ubuntu -c "bash /mnt/data/smrtlink/admin/bin/services-start"
 sudo sh -c "echo 'SMRT LINK services ready. Go to ${publicDNS}:9090' > /etc/motd"
 wall "SMRTLINK services ready. Go to ${publicDNS}:9090"
--- scripts/pacbio.sh
+++ scripts/pacbio.sh
@@ -11,10 +11,16 @@ sudo sh -c 'echo "* hard nproc 10000" >> /etc/security/limits.conf'
 sudo sh -c 'echo "* soft nofile 10000" >> /etc/security/limits.conf'
 sudo sh -c 'echo "* hard nofile 10000" >> /etc/security/limits.conf'
 
+#! /bin/bin
+
+set -e
+
 sudo sh -c 'aws s3 cp s3://czbiohub-scripts/other_software/pacbio-postlaunch.sh /var/lib/cloud/scripts/per-instance'
 sudo chmod 755 /var/lib/cloud/scripts/per-instance/pacbio-postlaunch.sh
 
 sudo sh -c "echo 'SMRT Link services not ready.' > /etc/motd"
 
-sudo apt-get --purge remove postgresql
-sudo apt-get install postgresql-9.6
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
+wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | sudo apt-key add -
+DEBIAN_FRONTEND=noninteractive sudo apt-get -y update
+DEBIAN_FRONTEND=noninteractive sudo apt-get -y install postgresql-9.6
